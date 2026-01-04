81af0337767216112e7d6e73cc48a4e53a6bba15
vttin22 <tin75vn@gmail.com>
2026-01-05 03:54:16 +0700

    api + web: add skip payment for dev

--- api/src/routes/v1/eTicketRoute.js
+++ api/src/routes/v1/eTicketRoute.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import { eTicketController } from '~/controllers/eTicketController'
 import { authMiddleware } from '~/middlewares/authMiddleware'
+import { optionalAuthMiddleware } from '~/middlewares/optionalAuthMiddleware'
 
 const Router = express.Router()
 
 Router.get('/bookings/:id/e-ticket', authMiddleware.isAuthorized, eTicketController.downloadETicket)
 Router.post('/bookings/:id/e-ticket/email', authMiddleware.isAuthorized, eTicketController.sendETicketEmail)
+
+// Public endpoint to confirm booking and send email for skip payment
+Router.post('/bookings/:id/confirm-and-email', optionalAuthMiddleware.tryAuthorize, eTicketController.confirmAndSendEmailPublic)
 
 export { Router as eTicketRoute }
